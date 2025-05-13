b9302dcfc97e77b8392971fd5f6e4cff7498f5a6
PaulMarren <paul.marren.coding@gmail.com>
2025-05-13 13:33:04 +0100

    Handle post request for collab form

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import About
 from .forms import CollaborateForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,6 +9,12 @@ def about_me(request):
     """
     Renders the About page
     """
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavor to respond within 2 working days.")
+
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
